64b8b41a23bdf99331e50af093239374b106fb08
pw <1040479402@qq.com>
2021-08-18 17:57:26 +0800

    2

--- p1/demo3.py
+++
@@ -1,4 +0,0 @@
-import pandas as pdimport numpy as npimport cufflinks as cfdf=pd.DataFrame
-(np.random.rand(12, 4), 
-columns=['a', 'b', 'c', 'd'])
-df.iplot(kind ='bar',title='示例', xTitle = 'X轴', yTitle ='Y轴')
